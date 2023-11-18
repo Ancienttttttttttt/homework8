67f53c2bbaf3838023799e03993f900d07387900
Ancienttttttttttt <pila33rus_RU@mail.ru>
2023-11-18 23:34:45 +0300

    123

---
+++ homework8/main.py
@@ -0,0 +1,9 @@
+#Zadacha 3
+# def sum_range(start = 1, end = 100):
+#     if start > end:
+#         start, end = end, start
+#     return sum(range(start, end + 1))
+# print(sum_range(1, 10))
+# print(sum_range(1, 100))
+# print(sum_range(10))
+# print(sum_range())
